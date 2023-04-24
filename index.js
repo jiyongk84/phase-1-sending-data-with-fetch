52c70540b1f0e6b57f5d6b56a94e8aa2b6964154
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-04-24 18:55:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {name, email}
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(userData)
+    }  
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then((userData) => { 
+        const newData = document.createElement('div')
+    document.querySelector('body').appendChild(newData)
+    newData.textContent = userData.id
+    })
+    .catch((message) => {
+        document.querySelector('body').append(message)
+})}
